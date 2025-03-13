a098110fc5c5b09021196f3be3af9b75f33ac1d0
smugii23 <smugii0203@gmail.com>
2025-03-13 16:27:21 -0700

    added healthz path and changed main path

--- main.go
+++ main.go
@@ -11,6 +11,13 @@ func main() {
 		Addr:    "localhost:8080",
 	}
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.HandleFunc("/healthz", healthHandler)
 	server.ListenAndServe()
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }

c294c91c70126943f345690c0e3700bb8e798f22
smugii23 <smugii0203@gmail.com>
2025-03-14 14:16:44 -0700

    added new middleware and handlers

--- main.go
+++ main.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
 func main() {
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Handler: mux,
 		Addr:    "localhost:8080",
 	}
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	fileServerWithPrefix := http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerWithPrefix))
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 	mux.HandleFunc("/healthz", healthHandler)
 	server.ListenAndServe()
 }

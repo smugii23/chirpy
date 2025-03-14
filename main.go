f26aa162f17d25f606c56e383323e223b5274d95
smugii23 <smugii0203@gmail.com>
2025-03-14 16:14:44 -0700

    added valid_chirp handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 	"sync/atomic"
@@ -18,11 +19,78 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))))
+	reqBody := `
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited ` + strconv.Itoa(int(cfg.fileserverHits.Load())) + ` times!</p>
+  </body>
+</html>
+`
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(reqBody))
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) valid_chirp(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Body string `json:"body"`
+	}
+
+	type validResp struct {
+		Valid bool `json:"valid"`
+	}
+
+	type errorResp struct {
+		Error string `json:"error"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	tweet := chirp{}
+	err := decoder.Decode(&tweet)
+	if err != nil {
+		resp := errorResp{
+			Error: "Something went wrong",
+		}
+		res, err := json.Marshal(resp)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+	if len(tweet.Body) > 140 {
+		resp := errorResp{
+			Error: "Chirp is too long",
+		}
+		res, err := json.Marshal(resp)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest) // 400 status code
+		w.Write(res)
+		return
+	}
+	resp := validResp{
+		Valid: true,
+	}
+	res, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func main() {
@@ -35,9 +103,10 @@ func main() {
 	fileServer := http.FileServer(http.Dir("."))
 	fileServerWithPrefix := http.StripPrefix("/app", fileServer)
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerWithPrefix))
-	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("/reset", apiCfg.resetHandler)
-	mux.HandleFunc("/healthz", healthHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.valid_chirp)
 	server.ListenAndServe()
 }
 
